2cab875114bbcff316015630ee1d7f764efb46f1
Venukanthan BS <itsvenu22@gmail.com>
2024-09-23 23:05:41 +0530

    added UI, CodeMirror, Admin, FrontEnd

--- codebin/app_snippets/admin.py
+++ codebin/app_snippets/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Snippet
 
-# Register your models here.
+@admin.register(Snippet)
+class SnippetAdmin(admin.ModelAdmin):
+    list_display = ('slug', 'created_at')  # You can customize which fields to show in the admin panel
+    search_fields = ('slug', 'content')  # Allow searching by slug and content
